d923d98cda05af16008979e654633c79d4d63f0d
Bogdan <bogdanklymenko@gmail.com>
2019-05-14 16:07:52 +0200

    1
    1

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>BERIG!</h2>
+      collect feedback
+    </div>
+  );
+};
+
+export default Landing;
